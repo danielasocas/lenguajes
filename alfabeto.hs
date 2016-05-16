80d1818e4baee7bab90a6f7981a53183696a8409
SergioTeranZ <sergioteran.zambrano@gmail.com>
2016-05-16 14:02:02 -0400

    Defini mejor el tipo Term, el alfabeto, los boleanos y comence a definir los operadores

--- alfabeto.hs
+++ alfabeto.hs
@@ -1,85 +1,52 @@
-{- Construccion del tipo de dato Var -}
+{- Data constructor Term -}
+data Term = Var Char
+	| T
+	| F
+	| Or Term Term
+	| And Term Term
+{- AQUI VAN LOS CASOS DE EQUIVALENCIA, NEGACION, Y OTRAS COSAS LOGICAS -}
 
-data Var = Var Char deriving (Show)
+{- Funciones para booleanos -}
+true :: Term; True = T
+false :: Term; False = F
 
-{- Construccion del tipo de dato Term -}
+{- Funciones de casos de Char-}
+a :: Term; a = Var 'a'
+b :: Term; b = Var 'b'
+c :: Term; c = Var 'c'
+d :: Term; d = Var 'd'
+e :: Term; e = Var 'e'
+f :: Term; f = Var 'f'
+g :: Term; g = Var 'g'
+h :: Term; h = Var 'h'
+i :: Term; i = Var 'i'
+j :: Term; j = Var 'j'
+k :: Term; k = Var 'k'
+l :: Term; l = Var 'l' 
+m :: Term; m = Var 'm'
+n :: Term; n = Var 'n'
+o :: Term; o = Var 'o'
+p :: Term; p = Var 'p'
+q :: Term; q = Var 'q'
+r :: Term; r = Var 'r'
+s :: Term; s = Var 's'
+t :: Term; t = Var 't'
+u :: Term; u = Var 'u'
+v :: Term; v = Var 'v'
+w :: Term; w = Var 'w'
+x :: Term; x = Var 'x'
+y :: Term; y = Var 'y'
+z :: Term; z = Var 'z'
 
-{- Funciones constantes para los terminos unarios-}
+{- Funciones de operadores -}
+(\/) :: Term -> Term -> Term ; (\/) a b = Or a b
+(/\) :: Term -> Term -> Term ; (/\) a b = And a b
+{-(<==>) :: Term -> Term -> Term ; (<==>) a b = And a b 	* Aqui va la declaracion del equivalente y su negado, 		*
+(!<==>) :: Term -> Term -> Term ; (!<==>) a b = And a b 	* que creo que tenemos que hacer una funcion aparte para eso* -}
 
-a :: Term
-a = Var 'a'
-
-b :: Term
-b = Var 'b'
-
-c :: Term
-c = Var 'c'
-
-d :: Term
-d = Var 'd'
-
-e :: Term
-e = Var 'e'
-
-f :: Term
-f = Var 'f'
-
-g :: Term
-g = Var 'g'
-
-h :: Term
-h = Var 'h'
-
-i :: Term
-i = Var 'i'
-
-j :: Term
-j = Var 'j'
-
-k :: Term
-k = Var 'k'
-
-l :: Term
-l = Var 'l'
-
-m :: Term
-m = Var 'm'
-
-n :: Term
-n = Var 'n'
-
-o :: Term
-o = Var 'o'
-
-p :: Term
-p = Var 'p'
-
-q :: Term
-q = Var 'q'
-
-r :: Term
-r = Var 'r'
-
-s :: Term
-s = Var 's'
-
-t :: Term
-t = Var 't'
-
-u :: Term
-u = Var 'u'
-
-v :: Term
-v = Var 'v'
-
-w :: Term
-w = Var 'w'
-
-x :: Term
-x = Var 'x'
-
-y :: Term
-y = Var 'y'
-
-z :: Term
-z = Var 'z'
+{- Ordenar precendencia de operadores -}
+infixl 2 (\/) 
+infixl 2 (/\) 
+infixl 1 (==>) 
+infixl 0 (<==>) 
+infixl 0 (!<==>) 
