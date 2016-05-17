72199c0697b500f3a2a0b57d14ffcdb110476705
SergioTeranZ <sergioteran.zambrano@gmail.com>
2016-05-17 16:40:37 -0400

    Actualizando con lo de meggie

--- sust.hs
+++ sust.hs
@@ -2,11 +2,19 @@ module Sust where
 
 Import Term
 
-data Sust  = sust Term Term
+data Sust  =  Term Term
 
-class sust s where
-	:: Term -> s -> Term
+class Sustitucion s where
+	sust :: Term -> s -> Term
+
+instance sustitucion Sust where
+sust a Sust 
+
+instance sust (Term,Sust,Term) where
+
+instance sust (Term,Term,Sust,Term,Term) where
+
 
 infixl 1 =: 
 (=:) :: Term -> Term -> Sust 
-(=:) a b  =  sust a b 
+(=:) a b  =   a b 
