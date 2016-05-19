fefba58aeb03405efb9846a2a793d9b385d0901c
SergioTeranZ <sergioteran.zambrano@gmail.com>
2016-05-19 00:07:38 -0400

    Modularice las funciones, para que se lea mas facil

---
+++ Main.hs
@@ -0,0 +1,73 @@
+{- 	Universidad Simon Bolivar
+	Laboratorio de Lenguajes I 
+	Verificador de demostraciones logicas
+
+	Daniela Socas 11-10979
+	Sergio Teran  11-11020
+
+	Codigo principal Ayudante logico
+-}
+
+module Main where
+
+import Sust
+import Term
+import Theorems
+import Show
+
+{- Funciones dummys -}
+with :: Ignorar; with = With
+
+lambda :: Ignorar; lambda = Lambda
+
+using :: Ignorar; using = Using
+
+{--------Funcion instantiate--------}
+{- 	izq, der 	= lado izquierdo y derecho de la ecuacion a instanciar (el Teorema).
+	s 			= es la sustitucion.
+-}
+instantiate :: (Sustituir s) => Ecuacion -> s -> Ecuacion
+instantiate (Equiv izq der) sus = Equiv (sust izq sus) (sust der sus) 
+
+{----------Funcion Leibniz----------}
+{-	izq, der 	= lado izquierdo y derecho de la ecuacion instanciada.
+	e			= Expresion a aplicar Leibniz.
+	z 			= Variable de la Expresion a la cual se va a sustituir con la ecuacion.
+-}
+leibniz :: Ecuacion -> Term -> Term -> Ecuacion
+leibniz (Equiv izq der) e (Var z) = Equiv (sust e (St izq (Var z))) (sust e (St der (Var z)))  
+
+{---------Funcion inferencia--------}
+infer :: (Sustituir s) => Float -> s -> Term -> Term -> Ecuacion
+infer n sus t1 e =  (leibniz (instantiate (prop n) sus) e t1)
+
+{----------- Funcion step ----------}
+{-	t1			= termino que compara en la inferencia
+	n,s,z,e 	= parametros de la funcion infer	
+	li,ld 		= termino que regresa si coincide con t1
+-}
+step :: (Sustituir s) =>  Term -> Float -> s -> Term -> Term -> Term 
+step t1 n sus (Var z) e	| t1 == li = li
+						| t1 == ld = ld
+						| otherwise = error "Regla de Inferencia no aplicable"
+						where
+							(Equiv li ld) = (infer n sus (Var z) e)
+
+{--------- Funcion statement -------}
+statement :: (Sustituir s, Show s) => Float -> Ignorar -> s -> Ignorar -> Ignorar -> Term -> Term -> Term -> IO Term
+statement f _ s _ _ (Var z) e t1 = do
+							{-putStrLn $ id "=== < statement " ++ show f ++ " with " ++ show s ++ " using lambda "++[z]++"("++show e ++ ") >"-}
+							let x = step t1 f s (Var z) e
+							putStrLn $ id show x
+							return (x)
+
+{----------- Funcion proof ---------}
+{-	a,b 		= terminos de la ecuacion a demostrar
+-}
+proof :: Ecuacion -> IO Term
+proof (Equiv a b) = do
+					putStrLn $ id "Probando " ++ (show a) ++ "==="++ (show b)
+					putStrLn $ id show a
+					return (a)
+
+{------------ Funcion done ---------}
--- Sust.hs
+++ Sust.hs
@@ -7,7 +7,6 @@
 
 	Sustitucion 
 -}
-
 {-# LANGUAGE FlexibleInstances #-}
 
 module Sust where
@@ -16,8 +15,12 @@ import Term
 import Theorems
 import Show
 
--- Clase de tipo para la sustitucion
+{------------- CONSTRUCTOR DE DATOS ------------}
+
+{--------------- Sust ----------------}
 data Sust = St Term Term -- St significando sustitucion textual
+
+{------------ COMPORTAMIENTO DE SUST -----------}
 
 -- Clase Sustitucion y funcion sust.
 class Sustituir s where
@@ -34,17 +37,17 @@ class Sustituir s where
 	sust F _ = F
 
 
-{- Instancia simple de sustitucion
-	a, b 	= parte de la ecuacion a la que voy a aplicar la sustitucion
+{--------- Instancia simple ----------}
+{-	a, b 	= parte de la ecuacion a la que voy a aplicar la sustitucion
 	t1	 	= terminos por los que voy a sustituir
 	p 	 	= terminos que quiero sustituir
 -}
 instance Sustituir Sust where
-	sust (Var a) (St t1 (Var p))	| a == p 	= t1 -- Caso base 
+	sust (Var a) (St t1 (Var p))| a == p 	= t1 -- Caso base 
 								| otherwise = Var a  
 
-{- Instancia doble de sustitucion
-	a, b	= parte de la ecuacion a la que voy a aplicar la sustitucion
+{--------- Instancia Doble ----------}
+{-	a, b	= parte de la ecuacion a la que voy a aplicar la sustitucion
 	t1, t2 	= terminos por los que voy a sustituir
 	p, q 	= terminos que quiero sustituir
 -}
@@ -54,12 +57,11 @@ instance Sustituir (Term,Sust,Term) where
 		| a == q 	= t2
 		| otherwise = Var a  
 
-{- Instancia doble de sustitucion
-	a, b 	 	= parte de la ecuacion a la que voy a aplicar la sustitucion
+{--------- Instancia Triple ----------}
+{-	a, b 	 	= parte de la ecuacion a la que voy a aplicar la sustitucion
 	t1, t2, t3	= terminos por los que voy a sustituir
 	p, q, r 	= terminos que quiero sustituir
 -}
--- Instancia triple de sustitucion
 instance Sustituir (Term,Term,Sust,Term,Term) where
 	sust (Var a) (t3, t2, (St t1 (Var p)), Var q , Var r)	
 		| a == p 	= t1 -- Caso base 
@@ -67,66 +69,7 @@ instance Sustituir (Term,Term,Sust,Term,Term) where
 		| a == r 	= t3
 		| otherwise = Var a  
 
--- Precedencia Izquierda 
-infixl 1 =: 
-(=:) :: Term -> Term -> Sust 
-(=:) = St  
-
-{- Funcion instantiate
-	izq, der 	= lado izquierdo y derecho de la ecuacion a instanciar (el Teorema).
-	s 			= es la sustitucion.
--}
-instantiate :: (Sustituir s) => Ecuacion -> s -> Ecuacion
-instantiate (Equiv izq der) sus = Equiv (sust izq sus) (sust der sus) 
-
-
-{- Funcion Leibniz
-	izq, der 	= lado izquierdo y derecho de la ecuacion instanciada.
-	e			= Expresion a aplicar Leibniz.
-	z 			= Variable de la Expresion a la cual se va a sustituir con la ecuacion.
--}
-leibniz :: Ecuacion -> Term -> Term -> Ecuacion
-leibniz (Equiv izq der) e (Var z) = Equiv (sust e (St izq (Var z))) (sust e (St der (Var z)))  
-
-{- Funcion inferrencia
-	
--}
-infer :: (Sustituir s) => Float -> s -> Term -> Term -> Ecuacion
-infer n sus t1 e =  (leibniz (instantiate (prop n) sus) e t1)
-
-{- Funcion step
-	t1			= termino que compara en la inferencia
-	n,s,z,e 	= parametros de la funcion infer	
-	li,ld 		= termino que regresa si coincide con t1
--}
-step :: (Sustituir s) =>  Term -> Float -> s -> Term -> Term -> Term 
-step t1 n sus (Var z) e	| t1 == li = li
-						| t1 == ld = ld
-						| otherwise = error "Regla de Inferencia no aplicable"
-						where
-							(Equiv li ld) = (infer n sus (Var z) e)
-
-{- Funcion statment
-
-
--}
-statment :: (Sustituir s) => Float -> Ignorar -> s -> Ignorar -> Ignorar -> Term -> Term -> Term -> IO Term
-statment f _ s _ _ (Var z) tE t1 = undefined
-
-{- Funciones dummys -}
-with :: Ignorar
-with = With
-
-lambda :: Ignorar
-lambda = Lambda
-
-using :: Ignorar
-using = Using
-
-{- Funcion proof
-
--}
-proof :: Ecuacion -> IO Term
-proof (Equiv a b) = do
-					putStrLn $ id "Probando " ++ (show a) ++ "==="++ (show b)
-					return (a)
+{------------------- OPERADOR ------------------}
+infixl 1 =:				{- Asociacion Izquierda -}
+(=:) :: Term -> Term -> Sust  	
+(=:) = St 
