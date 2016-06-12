43122ab713e462cc5747fa2b0cbed42465e9cf6f
SergioTeranZ <sergioteran.zambrano@gmail.com>
2016-06-12 12:15:49 -0400

    Cambie el Vn en la linea 19 por un "_", para quitar el warning del singleton

--- proyecto2.pl
+++ proyecto2.pl
@@ -16,7 +16,7 @@
 bienEtiquetado(A) :- bienEtiquetado_aux(A,[],[]).
 
 bienEtiquetado_aux(nodo(Vn,E),Lvn,Lva) :- \+ member((Vn),Lvn) , es_arista(Vn,E,Lvn,Lva).										
-es_arista(Vn,[],_,_).
+es_arista(_,[],_,_).
 es_arista(Vn0,[arista(Va,nodo(Vn1,E))|Es],Lvn,Lva) :- Va is abs(Vn0 - Vn1), \+ member((Va),Lva), bienEtiquetado_aux(nodo(Vn1,E),Vn0|Lvn,Va|Lva), es_arista(Vn0,Es,Vn0|Lvn,Va|Lva).	
 
 
