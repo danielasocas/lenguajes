26d07d70b98e01a78229b056825b3124edfde08a
SergioTeranZ <sergioteran.zambrano@gmail.com>
2016-06-13 15:06:19 -0400

    Cree funcion cantNodos, que cuneta la cantidad de nodos

--- proyecto2.pl
+++ proyecto2.pl
@@ -5,23 +5,24 @@
 *	Sergio Teran  11-11020
 */
 
-+/* Predicado principal
- +	Vn= Valor del nodo, E= Expresion, 
- +	Lvn = lista de vertices, Lva = lista de aristas
- + 	Revisa si no existe ya, si el arista es valido y agrega el Vn a la lista vertices
- +*/
- +
- +
- +bienEtiquetado(A) :- bienEtiquetado_aux(A,[],[]).
- +
- +bienEtiquetado_aux(nodo(Vn,E),Lvn,Lva) :- \+ member((Vn),Lvn) , es_arista(Vn,E,Lvn,Lva).										
- +es_arista(Vn,[],_,_).
- +es_arista(Vn0,[arista(Va,nodo(Vn1,E))|Es],Lvn,Lva) :- Va is abs(Vn0 - Vn1), \+ member((Va),Lva), bienEtiquetado_aux(nodo(Vn1,E),Vn0|Lvn,Va|Lva), es_arista(Vn0,Es,Vn0|Lvn,Va|Lva).	
- +
- +
- +
- +
- +
- +/*--------prueba------------*/
- +estudiar(daniela).
- +reprobar(X):- \+ estudiar(X).
+/* Predicado principal
+ 	Vn= Valor del nodo, E= Expresion, 
+ 	Lvn = lista de vertices, Lva = lista de aristas
+  	Revisa si no existe ya, si el arista es valido y agrega el Vn a la lista vertices
+ */
+ 
+/*Predicado que recorre un arbol y regresa la cantidad de nodos que tiene*/
+
+/*Caso Base*/
+cantNodos([],0).
+/*Caso recursivo*/
+cantNodos(nodo(_,Resto),N) :- cantNodos(Resto,M),N is M+1 .
+
+cantNodos([arista(_,SubArbol)|RestoA],N) :- cantNodos(SubArbol,M1), cantNodos(RestoA,M2), N is M1 + M2.
+
+
+ bienEtiquetado(A) :- bienEtiquetado_aux(A,[],[]).
+ 
+ bienEtiquetado_aux(nodo(Vn,E),Lvn,Lva) :- \+ member((Vn),Lvn) , es_arista(Vn,E,Lvn,Lva).										
+ es_arista(_,[],_,_).
+ es_arista(Vn0,[arista(Va,nodo(Vn1,E))|Es],Lvn,Lva) :- Va is abs(Vn0 - Vn1), \+ member((Va),Lva), bienEtiquetado_aux(nodo(Vn1,E),Vn0|Lvn,Va|Lva), es_arista(Vn0,Es,Vn0|Lvn,Va|Lva).	
