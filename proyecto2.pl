9319ef52ac453d4682d4970a650bba352da4ad94
Daniela Socas <danielasocasgil@gmail.com>
2016-06-14 07:42:14 -0400

    no sale bien la lista LvnOut

--- proyecto2.pl
+++ proyecto2.pl
@@ -7,22 +7,22 @@
 
 
 /* Predicado principal
-	Vn= Valor del nodo, E= Expresion, 
-	Lvn = lista de vertices, Lva = lista de aristas
- 	Revisa si no existe ya, si el arista es valido y agrega el Vn a la lista vertices
+	Revisa si no existe ya, si el arista es valido y agrega el Vn a la lista vertices
+	In: 
+		Vn= Valor del nodo, E= Etiqueta, 
+		Dos listas vacias, como empiean los Lvn y Lva del out.
+	Out:	
+		Lvn = lista de vertices, Lva = lista de aristas
 */
 
+bienEtiquetado(A) :- bienEtiquetado_aux(A,[],LvnOut,[],LvaOut), is_set(LvnOut), is_set(LvaOut),length(LvnOut,Tam), max_list(LvnOut,Tam).
 
-bienEtiquetado(A) :- bienEtiquetado_aux(A,[],[]).
+/*   */
 
-bienEtiquetado_aux(nodo(Vn,E),Lvn,Lva) :- \+ member((Vn),Lvn) , es_arista(Vn,E,Lvn,Lva).										
-es_arista(_,[],_,_).
-es_arista(Vn0,[arista(Va,nodo(Vn1,E))|Es],Lvn,Lva) :- Va is abs(Vn0 - Vn1), \+ member((Va),Lva), bienEtiquetado_aux(nodo(Vn1,E),Vn0|Lvn,Va|Lva), es_arista(Vn0,Es,Vn0|Lvn,Va|Lva).	
+bienEtiquetado_aux(nodo(Vn,[]),Lvn,[Vn|Lvn],Lva,Lva).
+bienEtiquetado_aux(nodo(Vn,E),Lvn,LvnOut,Lva,LvaOut) :- es_arista(Vn,E,[Vn|Lvn],LvnOut,Lva,LvaOut).
 
+/*   */
 
-
-
-
-/*--------prueba------------*/
-estudiar(daniela).
-reprobar(X):- \+ estudiar(X).
+es_arista(_,[],LvnOut,LvnOut,LvaOut,LvaOut).
+es_arista(Vn0,[arista(Va,nodo(Vn1,E))|Es],Lvn,LvnOut,Lva,LvaOut) :- Va is abs(Vn0 - Vn1), bienEtiquetado_aux(nodo(Vn1,E),Lvn,LvnOut,[Va|Lva],LvaOut), es_arista(Vn0,Es,LvnOut,_,LvaOut,_).
